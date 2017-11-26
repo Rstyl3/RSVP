55f964ea3b578dfe8b390bfe8b746750a30dd196
Severin Haselmann <overkill_2ooo@hotmail.com>
2017-11-26 15:28:40 +0100

    Toggling Edit State for Guests

--- src/Guest.js
+++ src/Guest.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import GuestName from './GuestName';
+
 const Guest = props => 
 
         <li>
-            <span>{props.name}</span>
+            <GuestName isEditing={props.isEditing}>
+               {props.name}
+            </GuestName>
             <label>
                 <input 
                     type="checkbox" 
@@ -12,7 +16,7 @@ const Guest = props =>
                     onChange={props.handleConfirmation}
                 /> Confirmed
             </label>
-            <button>edit</button>
+            <button onClick={props.handleToggleEditing}>edit</button>
             <button>remove</button>
         </li>
 ;
@@ -20,7 +24,9 @@ const Guest = props =>
 Guest.propTypes = {
     name: PropTypes.string.isRequired,
     isConfirmed: PropTypes.bool.isRequired,
-    handleConfirmation: PropTypes.func.isRequired
+    isEditing: PropTypes.bool.isRequired,
+    handleConfirmation: PropTypes.func.isRequired,
+    handleToggleEditing: PropTypes.func.isRequired
 };
 
 export default Guest;
